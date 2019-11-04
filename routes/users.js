dacbd392d4c8e3644baafa579fe7b2d3679d9e0c
Adel <htwr86@gmail.com>
2019-11-04 15:55:39 +0300

    validation fields in registration form

--- routes/users.js
+++ routes/users.js
@@ -7,8 +7,37 @@ router.get('/login', (req, res) => res.render('login'));
 // Register page
 router.get('/register', (req, res) => res.render('register'));
 
-// Register handler
+// Register handle
 router.post('/register', (req, res) => {
+  const { name, email, password, password2 } = req.body;
+  let errors = [];
+
+  //Check required fieds
+  if (!name || !email || !password || !password2) {
+    errors.push({ msg: 'Please fill in all fields' });
+  }
+
+  // Check passwords match
+  if (password != password2) {
+    errors.push({ msg: 'password are not match' });
+  }
+
+  // Check passwords length
+  if (password.length < 6) {
+    errors.push({ msg: 'password should be at least 6 characters' });
+  }
+  //console.log(errors);
+  if (errors.length > 0) {
+    res.render('register', {
+      errors,
+      name,
+      email,
+      password,
+      password2
+    });
+  } else {
+    res.send('pass');
+  }
   console.log(req.body);
   res.send('Hello');
 });
