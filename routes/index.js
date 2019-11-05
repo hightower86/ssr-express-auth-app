ca29a16bfa63911d3e43512484bd20b9865f9928
Adel <htwr86@gmail.com>
2019-11-05 20:57:27 +0300

    logout handle

--- routes/index.js
+++ routes/index.js
@@ -3,4 +3,7 @@ const router = express.Router();
 
 router.get('/', (req, res) => res.render('index'));
 
+// redirect to dashboard
+router.get('/dashboard', (req, res) => res.render('dashboard'));
+
 module.exports = router;
